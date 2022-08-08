fbc9a3c558e0bad96e5e74c0a652d1c60398a88c
John Hill <john.hill@oasisdigital.com>
2022-08-08 12:39:07 -0400

    updatePlayStats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int healthValue = 100;
+    public int itemsValue = 0;
+    public TMP_Text health;
+    public TMP_Text items;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateItems();
+        UpdateHealth();
+    }
+
+    void UpdateHealth()
+    {
+        health.text = string.Format("Health: {0}", healthValue);
+    }
+
+    void UpdateItems()
+    {
+        items.text = string.Format("Items:  {0}", itemsValue); //string.Format(itemsValue);
+    }
+
+}
