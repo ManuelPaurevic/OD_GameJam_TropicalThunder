b16a4947b506deadefeee5ca43f3b2181a6a3d4c
Drew Wiens <6962078+drewwiens@users.noreply.github.com>
2022-08-08 15:48:40 -0500

    fixes item picked up sound

--- Assets/Scripts/ItemPickupController.cs
+++ Assets/Scripts/ItemPickupController.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ItemPickupController : MonoBehaviour {
     [SerializeField]
     private PlayerStats playerStats;
+
+    private AudioSource pickupItemAudioSrc;
+
+    private void Awake() {
+
+        if (!pickupItemAudioSrc) {
+            List<AudioSource> audios = new List<AudioSource>();
+            GetComponents<AudioSource>(audios);
+            pickupItemAudioSrc = audios[1];
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D other) {
         if (other.tag == EntityTags.Item) {
             ItemController item = other.GetComponent<ItemController>();
             if (Vector2.Distance(transform.position, other.transform.position) < 0.5f) {
                 playerStats.IncrementCoconuts();
+                pickupItemAudioSrc.Play();
                 item.DestroyObject();
             }
         }
