427bb5915e70ca4147e2ff47c7bf16a9fe8a91fc
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-08-08 16:51:56 -0500

    Fast Fast

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,6 @@ public class GameManager : MonoBehaviour
     public bool isPaused = false;
     public GameObject pauseMenu;
     public GameObject endMenu;
-
-    public TMP_Text finalValue;
     // Start is called before the first frame update
     void Start()
     {
