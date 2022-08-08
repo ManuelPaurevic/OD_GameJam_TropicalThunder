89ceac8347480280c30753960e9e17e21025913d
Zachary Kipping <zjkipping@hotmail.com>
2022-08-08 11:49:33 -0500

    Added player basic dodge coroutine
    Dodging works pretty well, player movement stops though after dodge done
    
    
    Dodging fully works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,7 +12,19 @@ public class PlayerController : MonoBehaviour
     private float sprintSpeed = 5f;
 
     [SerializeField]
+    private float dodgeTime = 0.5f;
+
+    [SerializeField]
+    private float dodgeAmount = 10f;
+
+    [SerializeField]
     private MovementController movementController;
+
+    public bool dodging = false;
+
+    public Vector2 facingDirection;
+
+    private Vector2 lastMovement;
 
     private void Awake()
     {
@@ -26,7 +40,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnMovement(InputValue input) {
         Vector2 movement = input.Get<Vector2>();
-        movementController.ChangeMovement(movement);
+        lastMovement = movement;
+        if (!dodging) {
+            if (!Vector2.zero.Equals(movement)) {
+                facingDirection = movement;
+            }
+            movementController.ChangeMovement(movement);
+        }
     }
 
     private void OnSprint(InputValue input) {
@@ -36,6 +56,35 @@ public class PlayerController : MonoBehaviour
         } else {
             movementController.ChangeSpeed(walkSpeed);
         }
+    }
+
+    private void OnDodge() {
+        if (!Vector2.zero.Equals(facingDirection)) {
+            StartCoroutine(PerformDodge(facingDirection));
+        }
+    }
+
+    private IEnumerator PerformDodge(Vector2 direction) {
+        dodging = true;
+        float activeDodgeTime = 0;
+        movementController.ChangeMovement(direction);
+        while(activeDodgeTime <= dodgeTime) {
+            float dodgePercent;
+            if (activeDodgeTime == 0) {
+                dodgePercent = 0;
+            } else if (dodgeTime == activeDodgeTime) {
+                dodgePercent = 1f;
+            } else {
+                dodgePercent = activeDodgeTime / dodgeTime;
+            }
+            float dodgeSpeed = Mathf.Lerp(dodgeAmount, walkSpeed, dodgePercent);
+            movementController.ChangeSpeed(dodgeSpeed);
+            activeDodgeTime += Time.deltaTime;
+            yield return null;
+        }
+        movementController.ChangeSpeed(walkSpeed);
+        movementController.ChangeMovement(lastMovement);
+        dodging = false;
     }
 
     public Vector2 GetPosition() {
