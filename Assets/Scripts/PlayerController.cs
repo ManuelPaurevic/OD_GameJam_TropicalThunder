43e8a0947fedbe8139a2537fe8e8d4d7d7f795a4
Drew Wiens <6962078+drewwiens@users.noreply.github.com>
2022-08-09 09:54:17 -0500

    animates the player
    the sprites:
    https://rpgtileset.com/sprite/monkeys-sprite-1-for-rpg-maker-mv/

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,12 +16,30 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private Sprite leftSprite;
+
+    [SerializeField]
+    private Sprite leftSpriteLFoot;
+
+    [SerializeField]
+    private Sprite leftSpriteRFoot;
     [SerializeField]
     private Sprite upSprite;
     [SerializeField]
+    private Sprite upSpriteLFoot;
+    [SerializeField]
+    private Sprite upSpriteRFoot;
+    [SerializeField]
     private Sprite rightSprite;
     [SerializeField]
+    private Sprite rightSpriteLFoot;
+    [SerializeField]
+    private Sprite rightSpriteRFoot;
+    [SerializeField]
     private Sprite downSprite;
+    [SerializeField]
+    private Sprite downSpriteLFoot;
+    [SerializeField]
+    private Sprite downSpriteRFoot;
 
     [SerializeField]
     private float sprintSpeed = 5f;
@@ -48,9 +66,16 @@ public class PlayerController : MonoBehaviour {
 
     public Vector2 facingDirection;
 
+    private Vector2 lastFacingDirection;
+
     private Vector2 lastMovement;
 
     private float lastSpeed;
+
+    private int walkingAnimationFrameIdx = 0;
+    private int walkingAnimationFrameCounter = 0;
+
+    private int hitBlinkCounter = 0;
 
     private void Awake() {
         if (!movementController) {
@@ -71,6 +96,59 @@ public class PlayerController : MonoBehaviour {
     private void Start() {
         movementController.ChangeMovement(Vector2.zero);
         movementController.ChangeSpeed(walkSpeed);
+    }
+
+    private void UpdateWalkingAnimation() {
+        walkingAnimationFrameCounter = ++walkingAnimationFrameCounter % 8;
+        if (walkingAnimationFrameCounter == 0) {
+            walkingAnimationFrameIdx = ++walkingAnimationFrameIdx % 4;
+            if (Vector2.up.Equals(lastFacingDirection)) {
+                if (lastSpeed == sprintSpeed) {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? upSpriteLFoot : walkingAnimationFrameIdx == 1 ? upSpriteRFoot : walkingAnimationFrameIdx == 2 ? upSpriteLFoot : upSpriteRFoot;
+                } else {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? upSprite : walkingAnimationFrameIdx == 1 ? upSpriteLFoot : walkingAnimationFrameIdx == 2 ? upSprite : upSpriteRFoot;
+                }
+            } else if (Vector2.right.Equals(lastFacingDirection)) {
+                if (lastSpeed == sprintSpeed) {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? rightSpriteLFoot : walkingAnimationFrameIdx == 1 ? rightSpriteRFoot : walkingAnimationFrameIdx == 2 ? rightSpriteLFoot : rightSpriteRFoot;
+                } else {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? rightSprite : walkingAnimationFrameIdx == 1 ? rightSpriteLFoot : walkingAnimationFrameIdx == 2 ? rightSprite : rightSpriteRFoot;
+                }
+            } else if (Vector2.down.Equals(lastFacingDirection)) {
+                if (lastSpeed == sprintSpeed) {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? downSpriteLFoot : walkingAnimationFrameIdx == 1 ? downSpriteRFoot : walkingAnimationFrameIdx == 2 ? downSpriteLFoot : downSpriteRFoot;
+                } else {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? downSprite : walkingAnimationFrameIdx == 1 ? downSpriteLFoot : walkingAnimationFrameIdx == 2 ? downSprite : downSpriteRFoot;
+                }
+            } else if (Vector2.left.Equals(lastFacingDirection)) {
+                if (lastSpeed == sprintSpeed) {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? leftSpriteLFoot : walkingAnimationFrameIdx == 1 ? leftSpriteRFoot : walkingAnimationFrameIdx == 2 ? leftSpriteLFoot : leftSpriteRFoot;
+                } else {
+                    spriteRenderer.sprite = walkingAnimationFrameIdx == 0 ? leftSprite : walkingAnimationFrameIdx == 1 ? leftSpriteLFoot : walkingAnimationFrameIdx == 2 ? leftSprite : leftSpriteRFoot;
+                }
+            }
+        }
+    }
+
+    private void UpdateHitAnimation() {
+        if (hitBlinkCounter > 0) {
+            hitBlinkCounter--;
+            if (hitBlinkCounter % 6 < 3) {
+                spriteRenderer.color = Color.white;
+            } else {
+                spriteRenderer.color = Color.red;
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        // timeScale == 1f stops animating during potential pausing of time:
+        if (Time.timeScale == 1f) {
+            UpdateHitAnimation();
+            if (!dodging && !Vector2.zero.Equals(lastMovement)) {
+                UpdateWalkingAnimation();
+            }
+        }
     }
 
     private void UpdateFootstepsAudio(Vector2 movement) {
@@ -111,12 +189,16 @@ public class PlayerController : MonoBehaviour {
                 facingDirection = movement;
                 if (Vector2.up.Equals(facingDirection)) {
                     spriteRenderer.sprite = upSprite;
+                    lastFacingDirection = facingDirection;
                 } else if (Vector2.right.Equals(facingDirection)) {
                     spriteRenderer.sprite = rightSprite;
+                    lastFacingDirection = facingDirection;
                 } else if (Vector2.down.Equals(facingDirection)) {
                     spriteRenderer.sprite = downSprite;
+                    lastFacingDirection = facingDirection;
                 } else if (Vector2.left.Equals(facingDirection)) {
                     spriteRenderer.sprite = leftSprite;
+                    lastFacingDirection = facingDirection;
                 }
             }
             movementController.ChangeMovement(movement);
@@ -172,5 +254,7 @@ public class PlayerController : MonoBehaviour {
     public void Hit(int hitAmount) {
         playerHitAudioSrc.Play();
         playerStats.RemoveScore(hitAmount);
+        spriteRenderer.color = Color.red;
+        hitBlinkCounter = 18;
     }
 }
